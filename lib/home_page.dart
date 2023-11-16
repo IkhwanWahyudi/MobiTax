7e36f33d14d8a2da5881d2e1887c100aacc6ebae
Dhimas Eko 068 <dhimaseko00@gmail.com>
2023-11-16 22:46:57 +0800

    tambah icon

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,117 +12,222 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 240, 237, 237),
       appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(70, 152, 138, 1),
-        // backgroundColor: Colors.white,
+        backgroundColor: Color.fromRGBO(70,152,138, 1),
         title: const Text(
-          "MobiTax",
-          style: TextStyle(color: Colors.white),
+          "MOBITAX",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        elevation: 0,
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: IconButton(
+              onPressed: () {
+                // Buat ke setting
+              },
+              icon: Icon(
+                Icons.settings,
+                size: 30,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
         automaticallyImplyLeading: false,
+        //centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.only(top:16, bottom: 16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                height: 100,
-                padding: const EdgeInsets.all(6),
-                width: double.infinity,
-                // color: Colors.amber,
-                alignment: Alignment.centerLeft,
-                decoration: const BoxDecoration(
-                    color: const Color.fromRGBO(70, 152, 138, 1),
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10))),
-                child: const Text(
-                  "Hai, Nama",
-                  style: TextStyle(color: Colors.white),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              height: 110,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                //color: Color.fromRGBO(255,192,27, 1),
+                color: Color.fromRGBO(70,152,138, 1),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15),
                 ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey, // warna bayangan
+                    blurRadius: 5, // radius blur bayangan
+                    offset: Offset(0, 0), // pergeseran bayangan (horizontal, vertical)
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              Container(
-                height: 20,
-                // width: double.infinity,
-                width: lebar,
-                // color: Colors.green,
-                padding: const EdgeInsets.only(
-                  left: 6,
+              child: Container(
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(10),
+                width: 50,
+                height: 10,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
                 ),
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  "Kendaraan Anda",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  // Menambahkan fungsi GestureDetector untuk navigasi
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              DetailPage(), // Navigasi ke DetailPage
-                        ),
-                      );
-                    },
-                    child: Column(
-                      children: [
-                        Container(
-                          width: lebar,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            child: Text(
+                              "Dhimas Eko Wiyono",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
-                          padding: EdgeInsets.symmetric(vertical: 10.0),
-                          child: Column(
-                            children: [
-                              Text("Plat Nomor ${index + 1}"),
-                              Text("Kendaraan ${index + 1}"),
-                              Text("Masa Berlaku ${index + 1}")
-                            ],
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                      ],
+                          Container(
+                            child: Text(
+                              "Kota Samarinda",
+                              style: TextStyle(
+                                fontSize: 12,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  );
+                    IconButton(
+                      //padding: EdgeInsets.all(10),
+                      icon: Icon(
+                        Icons.edit,
+                        color: Colors.black,
+                        size: 30,
+                      ),
+                      onPressed: () {
+                        // Logika ketika tombol di tekan
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 20,
+              // width: double.infinity,
+              width: lebar,
+              // color: Colors.green,
+              // padding: const EdgeInsets.only(
+              //   left: 6,
+              // ),
+              margin: EdgeInsets.all(10),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                "Kendaraan Anda",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+
+            Expanded(
+                child : ListView.builder(
+                  shrinkWrap: true,
+                  //physics: const NeverScrollableScrollPhysics(),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    // Menambahkan fungsi GestureDetector untuk navigasi
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                DetailPage(), // Navigasi ke DetailPage
+                          ),
+                        );
+                      },
+                      child: Column(
+                        children: [
+                          Container(
+                            width: lebar,
+                            height: 90,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            // padding: EdgeInsets.symmetric(vertical: 15.0),
+                            padding: EdgeInsets.all(10),
+                            margin: EdgeInsets.only(left: 10,right: 10),
+                            // child: Column(
+                            //   children: [
+                            //     Text("Plat Nomor ${index + 1}"),
+                            //     Text("Kendaraan ${index + 1}"),
+                            //     Text("Masa Berlaku ${index + 1}")
+                            //   ],
+                            // ),
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 50,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Color.fromRGBO(70,152,138, 1),
+                                  ),
+                                  child: Icon(
+                                    Icons.directions_bike,
+                                    color: Colors.white,
+                                    size: 30,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(left: 10),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text("Plat Nomor ${index + 1}"),
+                                      Text("Kendaraan ${index + 1}"),
+                                      Text("Masa Berlaku ${index + 1}")
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                )
+            ),
+            Container(
+              margin: EdgeInsets.all(10), // Tambahkan margin pada Container jika diinginkan
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  // Logika ketika tombol ditekan
                 },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton.icon(
-                onPressed: () {},
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color.fromRGBO(70, 152, 138, 1),
-                  minimumSize:
-                      const Size(150, 50), // Atur lebar dan tinggi button
+                  minimumSize: const Size(150, 50), // Atur lebar dan tinggi button
                   padding: const EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 16), // Padding di sekitar icon dan teks
+                    vertical: 10,
+                    horizontal: 16,
+                  ), // Padding di sekitar icon dan teks
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(
-                        30.0), // Mengatur radius untuk membuat button rounded
+                      15.0, // Mengatur radius untuk membuat button rounded
+                    ),
                   ),
                 ),
                 icon: const Icon(Icons.add),
                 label: const Text('Tambah Kendaraan'),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       // bottomNavigationBar: SizedBox(
