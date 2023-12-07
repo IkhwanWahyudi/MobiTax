7bf07b2532a256dec2fff1e73e0ca3c543a8b2f7
S0q0u <harlinajengukirasih@gmail.com>
2023-12-07 22:15:29 +0800

    error handling regis

--- lib/auth.dart
+++ lib/auth.dart
@@ -3,27 +3,67 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class Auth {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  // Future<void> regis(String email, String password, String nama, String nik,
+  //     String alamat, String kecamatan, String kota) async {
+  //   final regisUser = await _auth.createUserWithEmailAndPassword(
+  //     email: email,
+  //     password: password,
+  //   );
+
+  //   final user = regisUser.user;
+  //   final uidPengguna = user?.uid;
+
+  // await FirebaseFirestore.instance
+  //     .collection('pengguna')
+  //     .doc(uidPengguna)
+  //     .collection('data_diri')
+  //     .add({
+  //   'nama': nama,
+  //   'nik': nik,
+  //   'alamat': alamat,
+  //   'kecamatan': kecamatan,
+  //   'kota': kota,
+  // });
+  // }
+
   Future<void> regis(String email, String password, String nama, String nik,
       String alamat, String kecamatan, String kota) async {
-    final regisUser = await _auth.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
+    try {
+      final regisUser = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
 
-    final user = regisUser.user;
-    final uidPengguna = user?.uid;
+      final user = regisUser.user;
+      final uidPengguna = user?.uid;
 
-    await FirebaseFirestore.instance
-        .collection('pengguna')
-        .doc(uidPengguna)
-        .collection('data_diri')
-        .add({
-      'nama': nama,
-      'nik': nik,
-      'alamat': alamat,
-      'kecamatan': kecamatan,
-      'kota': kota,
-    });
+      if (user != null) {
+        await user.updateDisplayName(user.email!);
+        print('User registered successfully with email: $email');
+
+        // MENYIMPAN AKUN KE FIREBASE
+        await FirebaseFirestore.instance
+            .collection('pengguna')
+            .doc(uidPengguna)
+            .collection('data_diri')
+            .add({
+          'nama': nama,
+          'nik': nik,
+          'alamat': alamat,
+          'kecamatan': kecamatan,
+          'kota': kota,
+        });
+      } else {
+        throw 'User registration failed';
+      }
+    } catch (e) {
+      print('Registrasi Error: $e');
+      if (e is FirebaseAuthException && e.code == 'email-already-in-use') {
+        throw 'Email sudah digunakan.';
+      } else {
+        throw 'Registrasi Error: $e';
+      }
+    }
   }
 
   Future<void> login(String email, String password) async {
